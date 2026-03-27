9fe29eb396c5231200f0e3eab577671cd9f370b9
montydragon06-art <montydragon06@gmail.com>
2026-03-27 19:03:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from email.mime.multipart import MIMEMultipart
 # --- 1. CONFIGURATION ---
 DATA_FILE = "agency_database.json"
 # Your Google Apps Script Web App URL
-TRACKER_URL = "https://script.google.com/macros/s/AKfycbwopXSz26Lv56blNumGcjFV-M9yCvFCzh6r5SK0dF7rBWDrA2R_3mow0B18JzDtQcfc/exec"
+TRACKER_URL = "https://script.google.com/macros/s/AKfycbw0mdkl4yfLLHQcDh4B6nDqi39N8ZyetIdcSMrt5lrTKwuLWtV4CfIKRdR5tGxUXlTz/exec"
 # Your Google Sheet ID (The long string in the browser URL of your sheet)
 SHEET_ID = "1fqMwLHV51IgbcjHM0y6rLIG1zciLPL7m_Z2gJ4ZA-tk"
 
